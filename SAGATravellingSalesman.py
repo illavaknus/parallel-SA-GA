dc5629d22e35c0ee1cabacc44b6b0011b801448f
Shashank Sunkavalli <shashanksunkavalli@gmail.com>
2013-05-13 07:02:26 -0700

    TSP modified

--- SAGATravellingSalesman.py
+++ SAGATravellingSalesman.py
@@ -2,7 +2,7 @@ import SAGAProblem
 import math
 import random
 import numpy as np
-import copy
+from copy import deepcopy
 
 """
 Class for defining the Travelling Salesman Problem
@@ -10,46 +10,47 @@ to be solved by SAGASolver using parallel SAGASolver
 """
 
 class SAGATravellingSalesman(SAGAProblem):
+	
 	"""
 	Instantiate a new SAGAProblem object
 	"""
 	def __init__(self, cities_list, dist_mat):
-		self.route = cities_list
-		self.distance_matrix = dist_mat
-		self.num_cities = len(cities_list)
-		self.energy = 0
-		self.max_iterations = 20 * num_cities
+		self._route = cities_list
+		self._dist_mat = dist_mat
+		self._num_cities = len(cities_list)
+		self._energy = 0
+		self._max_iterations = 20 * num_cities
 
 	"""
 	Calculates the energy of the SAGAProblem
 	"""
 	def get_energy(self):
 		total_distance = 0
-		for i in range(self.num_cities):
-			# print self.dist_matrix[route[i]][route[(i+1)%self.num_cities]]
+		for i in range(self._num_cities):
+			# print self.dist_matrix[route[i]][route[(i+1)%self._num_cities]]
 			if i == len(route) - 1:
 				total_distance += self.dist_matrix[route[i]][route[0]]
 			else:
 				total_distance += self.dist_matrix[route[i]][route[(i+1)]]
-		self.energy = total_distance
-		return self.energy
+		self._energy = total_distance
+		return self._energy
 	
 	"""
 	Creates a new candidate by performing up to
 	L different swaps on the SAGAProblem
 	"""
 	def generate_candidates(self, L):
-		new_route = copy.deepcopy(self.route)
-		new_dist_mat = copy.deepcopy(self.dist_matrix)
+		new_route = deepcopy(self._route)
+		new_dist_mat = deepcopy(self.dist_matrix)
 
 		if(L == None):
 			new_route = np.random.shuffle(new_route)
 		else:
-			assert(L <= self.num_cities)
+			assert(L <= self._num_cities)
 			i = 0
 			while i < L:
-				index_one = np.random.randint(0,self.num_cities)
-				index_two = np.random.randint(0,self.num_cities)
+				index_one = np.random.randint(0,self._num_cities)
+				index_two = np.random.randint(0,self._num_cities)
 				if index_two == index_one:
 					i -= 1
 				else:
@@ -63,4 +64,4 @@ class SAGATravellingSalesman(SAGAProblem):
 	SAGAProblem has been met 
 	"""
 	def criteria_fullfilled(self, iteration, energy):
-		return (iteration >= self.max_iterations)
+		return (iteration >= self._max_iterations)
